c0b9666ba97dc3333c3226b4deed22eb58e9c5fb
Nathaniel Rabinovich <Cody78n@gmail.com>
2018-09-03 22:23:33 -0400

    first commit

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyCNJC8N9iwoXMsPbfo3qpgtNLCbQMr6-xw",
+    authDomain: "firestore-example-30756.firebaseapp.com",
+    databaseURL: "https://firestore-example-30756.firebaseio.com",
+    projectId: "firestore-example-30756",
+    storageBucket: "firestore-example-30756.appspot.com",
+    messagingSenderId: "239908581774"
+};
